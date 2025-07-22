b54f22765de145b24190e730b9ad42a46b3f7944
Al-Amin Hossain <prodeveloperalamin@gmail.com>
2025-07-23 04:08:11 +0600

    react fundamental things

---
+++ src/Actor.jsx
@@ -0,0 +1,10 @@
+
+const Actor = ({name}) => {
+  return (
+    <div>
+      <li>Actor name is :{name} </li>
+    </div>
+  )
+}
+
+export default Actor
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import Actor from './Actor';
 import './App.css'
+import Todo from './Todo';
+
 function App() {
+  const actors =['sakib khan', 'shoriful raj', 'bapparaj', 'Pasha vai', 'shimul', "Salman shah"]
   return (
     <>
-      <h1 className="card">React learning </h1>
+    {actors.map(actor => <Actor name = {actor} ></Actor> )}
+    <Actor></Actor>
+
+
+
+
+
+
+
+
+
+    {/* <Todo num = "1" task ="learn React from Now" isDone={true} ></Todo>
+    <Todo num = "2" task ="Explore the core concenft from today" isDone={false} ></Todo>
+    <Todo num = "2" task ="Try Jsx" isDone={true} ></Todo>
       <Device></Device>
+      <Student name= "alamin" score="96" grade="13"></Student>
+      <Student name= "Hossain" score="98" grade="25"></Student>
+      <Student name= "full-stack developer" score="96" grade="3"></Student> */}
     </>
   )
 }
@@ -14,5 +34,21 @@ function Device() {
     <h2>This is device no </h2>
   </div>)
 }
+
+
+const {name, grade, score } ={name: "nothing", grade: '00', score: "00"};
+
+function Student({name, grade, score }) {
+  console.log(name)
+  return (
+    <div className='div2'>
+      <h3>This is student name is : {name} </h3>
+      <p>grade is {grade}</p>
+      <p>score is {score}</p>
+    </div>
+  )
+}
+
+
 
 export default App
